b996d41b4435cf8c804bd814caa9bb5441b54c4f
Fenikz31 <fenikz31@gmail.com>
2019-07-15 16:33:15 +0200

    End of the first sessions of simple node test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const bodyTableTag = document.querySelector("tbody");
 const tableTag = document.querySelector("table");
 
-function createBookRow(title, author,price) {
+function createBookRow({title, author, price, available}) {
+
     return `
-    <tr>
+    <tr class="table-${available ? "success" : "danger"}">
     <td>${title}</td>
     <td>${author}</td>
     <td>${price} €</td>
@@ -15,33 +16,66 @@ function render(parentTag, content) {
     parentTag.innerHTML += content;
 }
 
-const books = [
-    {
-        title: "Il était une fois",
-        author: "Michael DUPONT",
-        price: 15.44,
-        available: true
-    },
-    {
-        title: "Biographie d'un auteur",
-        author: "Emilie DURAND",
-        price: 6.88,
-        available: true
-    },
-    {
-        title: "Il était deux fois",
-        author: "Stéphanie BERTRAND",
-        price: 23.99,
-        available: false
-    },
-];
+let books = [];
 
-if (books.length >0) {
-    tableTag.style.display = "table";
+if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"));
+    buildTable();
+}
 
-    for (let book of books) {
-        const row = createBookRow(book.title, book.author, book.price);
-        render(bodyTableTag, row);
+fetch("./books.json").then(function (data) {
+    data.json().then(function (booksJson) {
+        books = booksJson;
+
+        buildTable();
+        saveData(booksJson);
+    });
+});
+
+// ################# AJAX Example ################# //
+// function getAjax() {
+    
+//     if (window.XMLHttpRequest) {
+//         return  new XMLHttpRequest();    
+//     } else {
+//         return new ActiveXObject("Microsoft.XMLHTTP");
+//     }
+// }
+
+// function sendRequest(url) {
+//     let xhr = getAjax();
+//     xhr.responseType = "json";
+
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === 4 && xhr.status ===200) {
+            
+//             console.log(xhr.readyState);
+//             console.log(xhr.response);
+            
+
+//         }
+//     };
+    
+//     xhr.open("GET", url);
+//     xhr.send();    
+// }
+// sendRequest("./books.json");
+//################################################# //
+function buildTable() {
+    
+    if (books.length >0) {
+        tableTag.style.display = "table";
+    
+        for (let book of books) {
+            const row = createBookRow(book);
+            render(bodyTableTag, row);
+        }
     }
 }
-                
+
+function saveData(data) {
+    if (window.localStorage) {
+        
+        localStorage.setItem("books", JSON.stringify(data))
+    }
+}
