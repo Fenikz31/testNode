def2ce48dff5e16baf5e87990b739305c95896cf
Fenikz <45063328+Fenikz31@users.noreply.github.com>
2019-07-16 14:57:30 +0200

    node test directory creation

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const fs = require("fs");
-const {join} = require ("path");
-const filename = join(__dirname, ".gitignore");
+const fs = require('fs');
+const { join, parse } = require('path');
+const { tmpdir } = require('os');
+// const filename = join(__dirname, '.gitignore');
+// const file = join(__dir, 'test');
 
-console.log(filename);
-fs.readFile(filename,function (error, content) {
+const dirTarget = join(tmpdir(), 'test');
+const filenameext = parse(__filename).base;
+
+console.log(dirTarget);
+fs.mkdir(dirTarget,function (error) {
 	if (!error) {
-		console.log(String(content));
+		fs.copyFile(__filename, dirTarget + "/copyIndex.js", function (error) {
+			if (!error) {
+				console.log(`Le fichier ${filenameext} a bien été copié dans le fichier de destination`)
+			} 
+		});
+	} else {
+		console.error("L'erreur suivante est survenue: " +error);
 	}
-	else console.error(error);
-})
+});
+
+// console.log(filename);
+// fs.readFile(filename, function(error, content) {
+// 	if (!error) {
+// 		console.log(String(content));
+// 	}
+// 	else console.error(error);
+// });
+
+// fs.readdir('.', function(error, files) {
+// 	console.log(String(files));
+// });
+
+// fs.copyFile(__filename, file, function(error) {
+// 	if (error === null) console.log(`La copie vers ${file} s'est bien passée`);
+// });
