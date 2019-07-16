b64ec111ce0d67abf36320af233c2f4ec06886e9
Fenikz <45063328+Fenikz31@users.noreply.github.com>
2019-07-16 16:22:30 +0200

    190716 module events

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-const fs = require('fs');
-const { join, parse } = require('path');
-const { tmpdir } = require('os');
-// const filename = join(__dirname, '.gitignore');
-// const file = join(__dir, 'test');
-
-const dirTarget = join(tmpdir(), 'test');
-const filenameext = parse(__filename).base;
-
-console.log(dirTarget);
-fs.mkdir(dirTarget,function (error) {
-	if (!error) {
-		fs.copyFile(__filename, dirTarget + "/copyIndex.js", function (error) {
-			if (!error) {
-				console.log(`Le fichier ${filenameext} a bien Ã©tÃ© copiÃ© dans le fichier de destination`)
-			} 
-		});
-	} else {
-		console.error("L'erreur suivante est survenue: " +error);
-	}
+const EventsEmitter = require("events");
+const emitter = new  EventEmitter();
+emitter.on("FILE_IS_READING", function () {
+    console.log("L'événement FILE_IS_READING a été détecté!");
 });
 
-// console.log(filename);
-// fs.readFile(filename, function(error, content) {
-// 	if (!error) {
-// 		console.log(String(content));
-// 	}
-// 	else console.error(error);
-// });
+emitter.emit("FILE_IS_READING");
 
-// fs.readdir('.', function(error, files) {
-// 	console.log(String(files));
-// });
-
-// fs.copyFile(__filename, file, function(error) {
-// 	if (error === null) console.log(`La copie vers ${file} s'est bien passÃ©e`);
-// });
+console.log(new Date(1983,4,2));
