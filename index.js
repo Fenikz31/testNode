7580f45f0dae138e7a124106e1417e7f0d4c70dd
Fenikz31 <fenikz31@gmail.com>
2019-07-16 11:36:47 +0200

    modules console & path

--- index.js
+++ index.js
@@ -1,101 +1,64 @@
-const bodyTableTag = document.querySelector("tbody");
-const tableTag = document.querySelector("table");
+const car = {
+	name: 'Peugeot',
+	color: 'Blue',
+	equipment: [ 'climatisation', 'airbag' ],
+	year: 2012,
+	available: true
+};
 
-function createBookRow({title, author, price, available}) {
+console.log(car);
+console.log();
 
-    return `
-    <tr class="table-${available ? "success" : "danger"}">
-    <td>${title}</td>
-    <td>${author}</td>
-    <td>${price} €</td>
-    </tr>
-    `    ;
-}
+console.log('La voiture est une ' + car.name);
+console.log('La voiture est de couleur ', car.color);
+console.log(`Elle date de ${car.year}`);
 
-function render(parentTag, content) {
-    parentTag.innerHTML += content;
-}
+console.log();
+// CHAINES FORMATEES :
+// %s --> string
+// %d --> number
+// %j --> json
+// %O --> object (depth = 3 max)
+// %o --> object (depth = 5 max)
+console.log("elle est équipé de la %s et de l'%s", car.equipment[0], car.equipment[1]);
+console.log('JSON: %j', car);
+console.log(JSON.stringify(car));
 
-let books = [];
-buildTable();
+console.log();
+console.log('%o', { a: { b: { c: { d: { e: { f: 'f' } } } } } });
+console.log('%O', { a: { b: { c: { d: { e: { f: 'f' } } } } } });
 
-function getbooks() {
+console.time("myTimer");
+for (let i = 0; i < 20000; i++);
+console.timeEnd("myTimer");
 
-    if (windows.localStorage && localStorage.getItem("books")) {
-        try {
-            books = JSON.parse(localStorage.getItem("books"));
-        } catch (e) {
-            return [];
-        }
-    }
-    return [];
-}
-    
-fetch("./books.json").then(function (response) {
-    response.json().then(function (booksJson) {
-        saveData(booksJson);
+const fs = require("fs");
+const stdout = fs.createWriteStream("./stdout.log");
+const stderr = fs.createWriteStream("./stderr.log");
 
-        if (books.length !== booksJson.length) {            
-            books = booksJson;
-            buildTable();
-        } else {
-            for ( let book of books) {
-                const bookJson = booksJson.filter(function (b){
-                    return b.title === book.title && b.author === book.author;
-                })[0];
-                if (bookJson.available !== book.available || bookJson.price === book.price) {
-                    books = booksJson;
-                    buildTable();
-                }
-            }
-        }
-    });
-});
+const Console = require("console").Console;
+const logger = new Console({stdout, stderr});
 
-// ################# AJAX Example ################# //
-// function getAjax() {
-    
-//     if (window.XMLHttpRequest) {
-//         return  new XMLHttpRequest();    
-//     } else {
-//         return new ActiveXObject("Microsoft.XMLHTTP");
-//     }
-// }
+console.log();
+logger.log("Salut les amis!");
+logger.error("Mince une erreur est survenue!");
 
-// function sendRequest(url) {
-//     let xhr = getAjax();
-//     xhr.responseType = "json";
+const path = require('path');
 
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status ===200) {
-            
-//             console.log(xhr.readyState);
-//             console.log(xhr.response);
-            
+console.log(path.dirname('/index.js'));
+console.log(path.basename('/index.js'));
+console.log(path.extname('/index.js'));
 
-//         }
-//     };
-    
-//     xhr.open("GET", url);
-//     xhr.send();    
-// }
-// sendRequest("./books.json");
-//################################################# //
-function buildTable() {
-    
-    if (books.length >0) {
-        tableTag.style.display = "table";
-    
-        for (let book of books) {
-            const row = createBookRow(book);
-            render(bodyTableTag, row);
-        }
-    }
-}
+console.log();
+console.log(path.sep);
+console.log(path);
 
-function saveData(data) {
-    if (window.localStorage) {
-        
-        localStorage.setItem("books", JSON.stringify(data))
-    }
-}
+console.log();
+const from_to = path.relative('..', '.');
+const absolutePath = path.resolve('.', 'abc', 'test.txt');
+
+console.log(from_to);
+console.log(absolutePath);
+
+console.log();
+console.log(path.parse('/abc.txt'));
