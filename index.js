101c73bc6a0f7d189574c5efad050ff4a7e8006f
Fenikz31 <fenikz31@gmail.com>
2019-07-17 09:39:02 +0200

    Promises in NodeJs

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const EventsEmitter = require("events");
-const emitter = new  EventsEmitter();
+// Stocké sur un serveur distant
+const user = {
+    name: "Marcus",
+    email: "marcus@mail.com",
+    role: "ADMIN",
+    age: 55,
+};
+// ------------- //
 
-const months = ["Janvier","Février","Mars","Avril","Mai","Juin", "Juillet", "Août","Septembre", "Octobre", "Novembre", "Décembre"];
+function getUser(username) {
+    // Connexion serveur distant
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
 
-emitter.on("date", function (date) {
-    console.log("Date: %d %s %d", date.getDate(),months[date.getMonth()], date.getFullYear());
+            if (user.name.toLowerCase() === username.trim().toLowerCase()) {
+                resolve(user);
+            } else {
+                reject("Il y'a eu un problème lors de la connexion à la BDD!");
+            }
+        }, 3000);
+    });
+}
+
+const u2 = getUser("Marcus");
+const u1 = getUser("Michael");
+
+getUser('michael')
+  .then(data => console.log(data))
+  .catch(e => console.error(e));
+
+getUser('Marcus')
+  .then(data => console.log(data))
+  .catch(e => console.error(e));
+
+u1.then(function (data) {
+    console.log(data);
+}).catch(function (error) {
+    console.error(error);
 });
 
-emitter.emit("date", new Date(1983,4,2));
-console.log(new Date(1983,4,2));
+u2.then(function (data) {
+    console.log(data);
+}).catch(function (error) {
+    console.error(error);
+});
 
-emitter.emit("date", new Date());
-console.log(new Date());
+// const sayHello = name => console.log(`Salut ${name}!`);
+
+// sayHello();
