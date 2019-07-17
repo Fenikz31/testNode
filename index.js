15dd04e3f446b7244b4bdbc0f9799d50d66b32ad
Fenikz31 <fenikz31@gmail.com>
2019-07-17 09:51:15 +0200

    Promises in NodeJs - suite

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-// Stocké sur un serveur distant
-const user = {
-    name: "Marcus",
-    email: "marcus@mail.com",
-    role: "ADMIN",
-    age: 55,
-};
-// ------------- //
+const util = require("util");
+const {promisify} = require("util");  // const promisify = require("util").promisify;
 
-function getUser(username) {
-    // Connexion serveur distant
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
+const {readdir} = require("fs");
+const readdirPromise = promisify(readdir);
 
-            if (user.name.toLowerCase() === username.trim().toLowerCase()) {
-                resolve(user);
-            } else {
-                reject("Il y'a eu un problème lors de la connexion à la BDD!");
-            }
-        }, 3000);
-    });
-}
-
-const u2 = getUser("Marcus");
-const u1 = getUser("Michael");
-
-getUser('michael')
-  .then(data => console.log(data))
-  .catch(e => console.error(e));
-
-getUser('Marcus')
-  .then(data => console.log(data))
-  .catch(e => console.error(e));
-
-u1.then(function (data) {
-    console.log(data);
-}).catch(function (error) {
-    console.error(error);
+readdir(".",(err,files)=>{
+    if(err) return;
+    console.log(files);
 });
 
-u2.then(function (data) {
-    console.log(data);
-}).catch(function (error) {
-    console.error(error);
-});
-
-// const sayHello = name => console.log(`Salut ${name}!`);
-
-// sayHello();
+readdirPromise.then(files => console.log(files)).catch(e => console.error(e));
